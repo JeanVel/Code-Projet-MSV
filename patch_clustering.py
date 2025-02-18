24c5db25fd510058e9c6aca815b12e24a492aa82
simon <simon.bure@mailo.com>
2025-02-19 00:29:39 +0100

    Sauvegarde du résultat du clustering

--- patch_clustering.py
+++ patch_clustering.py
@@ -10,13 +10,8 @@ def get_positions_from_npz(paths):
     return position_x, position_y
 
 
-# Exemple : Génération d'un nuage de points aléatoire
-# np.random.seed(42)
-# x = np.random.rand(100) * 10  # Positions x
-# y = np.random.rand(100) * 10  # Positions y
-
-paths = ["generated/plants_last_x_positions.npz", "generated/plants_last_y_positions.npz"]
-x, y = get_positions_from_npz(paths)
+paths_positions = ["generated/plants_last_x_positions.npz", "generated/plants_last_y_positions.npz"]
+x, y = get_positions_from_npz(paths_positions)
 points = np.column_stack((x, y))  # Fusionner x et y en une seule liste de points (N, 2)
 
 # Appliquer DBSCAN
@@ -28,12 +23,8 @@ n_clusters = len(set(labels))
 
 # Taille des clusters
 unique_labels, counts = np.unique(labels, return_counts=True)
-print(unique_labels)
-print(counts)
-cluster_sizes = dict(zip(unique_labels, counts))
-
 print(f"Nombre de clusters détectés : {n_clusters}")
-print(f"Taille de chaque cluster : {cluster_sizes}")
+print(f"Taille de chaque cluster : {counts}")
 
 # Tracer le nuage de points avec les clusters
 plt.scatter(x, y, c=labels, cmap='viridis', edgecolor='k')
@@ -41,10 +32,9 @@ plt.colorbar(label="Cluster ID")
 plt.title(f"Clustering DBSCAN - {n_clusters} clusters détectés")
 plt.xlabel(r"$X$", size=14)
 plt.ylabel(r"$Y$", size=14)
-plt.show()
+plt.savefig("figures/graphiques/patchs_clustering.png")
 
 # Tracer l'histogramme du nombre de clusters en fonction de leur taille
 plt.hist(counts, edgecolor='k')
 plt.xlabel("Taille des patchs", size=14)
 plt.ylabel("Nombre de patchs", size=14)
-plt.show()
