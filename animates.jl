d85da6e10cbc329415c5d1984aaba28a0e94389a
simon <simon.bure@mailo.com>
2025-03-09 10:18:12 +0100

    Animation densité fonctionne

--- animates.jl
+++ animates.jl
@@ -20,6 +20,33 @@ function animates_plants(plants_positions, dom, frame_step)
 end
 
 
+function animates_g_water_density(g_water_positions, dom, frame_step)
+    g_water_x, g_water_y = g_water_positions
+
+    p = plot(legend=:topright, xlim=(-2 * dom, 2 * dom), ylim=(-2 * dom, 2 * dom), xlabel="x", ylabel="y", title="Simulation de l'eau de sous-sol")
+    frame_number = length(g_water_x)
+
+    anim = @animate for frame_number in 1:frame_step:frame_number
+        if length(g_water_x[frame_number]) > 0
+            g_water_density_estimation = kde((g_water_x[frame_number], g_water_y[frame_number]))
+            heatmap(
+                p, g_water_density_estimation.x, g_water_density_estimation.y, g_water_density_estimation.density',
+                color=:plasma, xlabel="X", ylabel="Y", label="Densité d'eau dans le sol",
+                xlims=(-2 * dom, 2 * dom), ylims=(-2 * dom, 2 * dom)
+            )
+        else
+            heatmap(
+                p, [-2 * dom, 2 * dom], [-2 * dom, 2 * dom], fill(0.0, 2, 2)',
+                color=:plasma, xlabel="X", ylabel="Y", label="Densité d'eau dans le sol",
+                xlims=(-2 * dom, 2 * dom), ylims=(-2 * dom, 2 * dom)
+            )
+        end
+    end
+
+    return anim
+end
+
+
 function create_animation(positions_x, positions_y, dom, frame_step=5)
     plants_x = positions_x[1]
     plants_y = positions_y[1]
@@ -34,7 +61,7 @@ function create_animation(positions_x, positions_y, dom, frame_step=5)
     p2 = plot(legend=:topright, xlim=(-2 * dom, 2 * dom), ylim=(-2 * dom, 2 * dom), xlabel="x", ylabel="y", title="Simulation de l'eau de surface")
 
     frame_number = size(plants_x, 1)
-    print("FRAME NUMBER: ", frame_number)
+    println("FRAME NUMBER: ", frame_number)
     anim = @animate for frame_number in 1:frame_step:frame_number
         if length(g_water_x[frame_number]) > 0
             g_water_density_estimation = kde((g_water_x[frame_number], g_water_y[frame_number]))
@@ -75,13 +102,14 @@ end
 
 
 @load "generated/plants_ts.jld2" plants_x_ts plants_y_ts
-# @load "generated/ground_water_ts.jld2" g_water_x_ts g_water_y_ts
-# @load "generated/surface_water_ts.jld2" s_water_x_ts s_water_y_ts
+@load "generated/ground_water_ts.jld2" g_water_x_ts g_water_y_ts
+@load "generated/surface_water_ts.jld2" s_water_x_ts s_water_y_ts
 
 
 # Création de l'animation
 # anim = animates_plants([plants_x_ts, plants_y_ts], 2, 10)
-anim = create_animation([plants_x_ts[1:500], g_water_x_ts[1:500], s_water_x_ts[1:500]], [plants_y_ts[1:500], g_water_y_ts[1:500], s_water_y_ts[1:500]], 2, 5)
+anim = animates_g_water_density([g_water_x_ts, g_water_y_ts], 2, 10)
+# anim = create_animation([plants_x_ts[1:500], g_water_x_ts[1:500], s_water_x_ts[1:500]], [plants_y_ts[1:500], g_water_y_ts[1:500], s_water_y_ts[1:500]], 2, 5)
 println("ANIMATION CREATED")
 gif(anim, "figures/animations/density.gif", fps=10)
 println("ANIMATION SAVED")
